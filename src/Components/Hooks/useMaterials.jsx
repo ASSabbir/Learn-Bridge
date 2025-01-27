3f11a2a17bfcb5647acbdcbe18ac90d528e9ee75
ASSabbir <atiksabbir1125@gmail.com>
2025-01-28 02:26:56 +0600

    added some section

--- src/Components/Hooks/useMaterials.jsx
+++ src/Components/Hooks/useMaterials.jsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useContext } from 'react';
+import  { useContext } from 'react';
 import { AuthContext } from '../Main/AuthProvider';
 
 const useMaterials = () => {
-    const {user, loading}=useContext(AuthContext);
+    const {user}=useContext(AuthContext);
     
     const {data:materials=[],isLoading,refetch }=useQuery({
         queryKey: ['tuitor-session'],
